9785c9462c3e665183de8913a52838efe5cea95a
Naomi Ch <naomi.chika.1@gmail.com>
2021-09-08 19:19:39 +0300

    add class source and define the obkects

---
+++ app/models.py
@@ -0,0 +1,17 @@
+class Source:
+  '''
+  Source class to define Source objects
+  '''
+  def __init__(self,id,name,url): #should we add category? 
+    self.id = id
+    self.name = name
+    self.url = url
+
+
+
+
+#class Article:
+  #'''
+  #Article class to define Article objects
+  #'''
+  
