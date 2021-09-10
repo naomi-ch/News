372313e3e59d78b6076d4f53cac60d580ef47691
Naomi Ch <naomi.chika.1@gmail.com>
2021-09-10 16:29:37 +0300

    add description and url to Source class objects

--- app/models.py
+++ app/models.py
@@ -2,9 +2,10 @@ class Source:
   '''
   Source class to define Source objects
   '''
-  def __init__(self,id,name,url): #should we add category? 
+  def __init__(self,id,name,description,url): #should we add category? 
     self.id = id
     self.name = name
+    self.description = description
     self.url = url
 
 
