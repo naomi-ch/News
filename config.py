0440a4dd899dad03b462a659363e09fd4f8d53d7
Naomi Ch <naomi.chika.1@gmail.com>
2021-09-10 16:24:50 +0300

    add base url,api key + secret key to config class

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+from instance.config import NEWS_API_KEY, SECRET_KEY
 import os 
 
 class Config:
     '''
     General configuration parent class
     '''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
